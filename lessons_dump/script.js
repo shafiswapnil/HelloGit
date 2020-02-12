043188b250715e899d453917e0d036d1b4717bc5
ShafiAhmed <shafiahmedswapnil6494@gmail.com>
2020-02-13 00:35:35 +0600

    summation using math.reduce

--- lessons_dump/script.js
+++ lessons_dump/script.js
@@ -1,9 +1,10 @@
-var numbers = [1,2,3,4,5];
+var numbers = [1,2,3,4,5,6,7];
 
 function sum(n) {
 	var summation = 0;
 
 	// do the math
+	summation = n.reduce(function (o,n) { return o + n ; });
 	
 	return summation;
 }
