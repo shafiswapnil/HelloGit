f40b074f33a21d4ac44a8965b3e27adfe676123c
ShafiAhmed <shafiahmedswapnil6494@gmail.com>
2020-03-19 14:50:23 +0600

    some fixes

--- lessons_dump/script.js
+++ lessons_dump/script.js
@@ -1,12 +1,14 @@
-var numbers = [1,2,3,4,5,6,7];
+var numbers = [1, 2, 3, 4, 5, 6, 7];
 
 function sum(n) {
-	var summation = 0;
+  var summation = 0;
 
-	// do the math
-	summation = n.reduce(function (o,n) { return o + n ; });
-	
-	return summation;
+  // do the math
+  summation = n.reduce(function(o, n) {
+    return o + n;
+  });
+
+  return summation;
 }
 
-console.log( sum ( numbers ) );
+console.log(sum(numbers));
